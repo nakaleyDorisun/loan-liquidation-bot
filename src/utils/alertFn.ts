375f8a320fab506f5e765941533cd2afb5506c0e
nakaleyDorisun <audioreklama39@gmail.com>
2025-05-17 13:13:46 +0400

    before LVT - LTV fix

--- src/utils/alertFn.ts
+++ src/utils/alertFn.ts
@@ -1,38 +1,61 @@
 import { getCoinByID } from "@/api/getCoinByID";
 import { getLVT } from "@/utils/getLVT";
 import { MyContext } from "@/types/types";
+import { createInlineKeyboard } from "@/keyboards/createInlineKeyboard";
 
 export const alertFn = async (ctx: MyContext, id: string) => {
-  const userID = ctx.session.user;
-  const loan = ctx.session.loans.filter((loan) => loan.id === id);
+  try {
+    const userID = ctx.session.user;
+    const loan = ctx.session.loans.filter((loan) => loan.id === id);
 
-  const borrowCoinId = loan[0].borrowCoinId;
-  const borrowCoinPrice = await getCoinByID(ctx, borrowCoinId);
-  const borrowCoinAmount = loan[0].borrowCoinAmount;
+    const borrowCoinId = loan[0].borrowCoinId;
+    const borrowCoinSymbol = loan[0].borrowCoinSymbol;
+    const borrowCoinPrice = await getCoinByID(ctx, borrowCoinId);
+    const borrowCoinAmount = loan[0].borrowCoinAmount;
+    const borrowCoinCost = borrowCoinPrice! * borrowCoinAmount; // if?
 
-  const collateralCoinId = loan[0].collateralCoinId;
-  const collateralCoinPrice = await getCoinByID(ctx, collateralCoinId);
-  const collateralCoinAmount = loan[0].collateralCoinAmount;
+    const collateralCoinId = loan[0].collateralCoinId;
+    const collateralCoinSymbol = loan[0].collateralCoinSymbol;
+    const collateralCoinPrice = await getCoinByID(ctx, collateralCoinId);
+    const collateralCoinAmount = loan[0].collateralCoinAmount;
 
-  const alertLVT = loan[0].alertLVT;
+    const alertLVT = loan[0].alertLVT;
 
-  const alert = setInterval(async () => {
-    let currentTLV = 0;
-    if (borrowCoinPrice && collateralCoinPrice) {
-      currentTLV = getLVT(
-        borrowCoinPrice,
-        borrowCoinAmount,
-        collateralCoinPrice,
-        collateralCoinAmount
-      );
-    }
-    if (currentTLV > alertLVT && userID) {
-      await ctx.api.sendMessage(
-        userID,
-        `эй пидор, проснись, твой currentTLV ${currentTLV}, а это выше ${alertLVT}, тоби пизда (почти)`
-      );
-    }
-  }, 10000);
-  const length = ctx.session.loans.length;
-  ctx.session.loans[length - 1].alertInterval = alert;
+    const alert = setInterval(async () => {
+      let currentLTV = 0;
+      if (borrowCoinPrice && collateralCoinPrice) {
+        currentLTV = getLVT(
+          borrowCoinPrice,
+          borrowCoinAmount,
+          collateralCoinPrice,
+          collateralCoinAmount
+        );
+      }
+      if (currentLTV > alertLVT && userID) {
+        const buttons = [
+          {
+            text: "Выключить🔕",
+            callback_data: "loans",
+          },
+        ];
+        const keyboard = await createInlineKeyboard(buttons);
+        await ctx.api.sendMessage(
+          userID,
+          `Эй пидор, проснись🦄🎊🎉\nпо твоему займу ${borrowCoinSymbol}-${collateralCoinSymbol}🍀🍀🍀\nна сумму ${borrowCoinCost}$\ncurrentLTV ${currentLTV}🌸🌸🌸\nа это выше ${alertLVT}🐶🐱🐼\nтоби пизда (почти)💖💘🌈`,
+          {
+            reply_markup: keyboard,
+            parse_mode: "HTML",
+          }
+        );
+        await ctx.api.sendSticker(
+          userID,
+          "CAACAgIAAxkBAAEOgkZoKE4RNTznT5dSCsc3jRnM7K4eKgACRxkAAjVw0EvfFb2al0cQIjYE"
+        );
+      }
+    }, 10000);
+    const length = ctx.session.loans.length;
+    ctx.session.loans[length - 1].alertInterval = alert;
+  } catch (error) {
+    console.log(error);
+  }
 };
